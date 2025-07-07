fa027ca76a4cec34ccd21d86b9f36684257c1e67
BoryanaPetkova-eng <130145291+BoryanaPetkova-eng@users.noreply.github.com>
2025-07-07 16:54:06 +0300

    add @Builder for flexible object creation

--- ecoSurvey-svc/src/main/java/app/web/dto/SurveyRequest.java
+++ ecoSurvey-svc/src/main/java/app/web/dto/SurveyRequest.java
@@ -3,12 +3,14 @@ package app.web.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SurveyRequest {
