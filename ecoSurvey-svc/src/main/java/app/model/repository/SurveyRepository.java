7a67caaedfe2ddfc34d056fcac8b2b89824f336e
BoryanaPetkova-eng <130145291+BoryanaPetkova-eng@users.noreply.github.com>
2025-07-07 16:52:48 +0300

    add countVotesBySubject JPQL query

--- ecoSurvey-svc/src/main/java/app/model/repository/SurveyRepository.java
+++ ecoSurvey-svc/src/main/java/app/model/repository/SurveyRepository.java
@@ -2,8 +2,10 @@ package app.model.repository;
 
 import app.model.model.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +13,8 @@ import java.util.UUID;
 public interface SurveyRepository extends JpaRepository<Survey, UUID> {
 
     Optional<Survey> findByUserId(UUID userId);
+
+    @Query("SELECT s.subject, COUNT(s) FROM Survey s GROUP BY s.subject")
+    List<Object[]> countVotesBySubject();
 
 }
